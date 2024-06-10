f820936445a138232ecd107928682081ad70bdb4
mariosajdini <t8200003@aueb.gr>
2024-06-10 18:29:50 +0300

    Create retriever.

--- backend/app/services/search.py
+++ backend/app/services/search.py
@@ -1,5 +1,7 @@
 from qdrant_client import QdrantClient, models
+from langchain_qdrant import Qdrant
 from backend.app.db.database_connection import DatabaseConnection
+from backend.app.services.embedding import EmbeddingService
 
 
 class SearchService:
@@ -7,6 +9,8 @@ class SearchService:
         self.connection = DatabaseConnection()
         self.connection.connect()
         self.client = self.connection.connection
+        self.embedding_service = EmbeddingService(model=None, embedding_service="openai",
+                                                  qdrant_client=self.client).embedding_client
 
     def search(self, collection_name, tenant_id, query_vector, limit):
         query_filter = models.Filter(
@@ -25,3 +29,21 @@ class SearchService:
             query_vector=query_vector,
             limit=limit,
         )
+
+    def get_tenant_retriever(self, collection_name, tenant_id, score_threshold=0.5, limit=1):
+        qdrant = Qdrant(self.client, collection_name, self.embedding_service)
+        filter = models.Filter(
+            must=[
+
+                models.FieldCondition(
+                    key="tenant_id",
+                    match=models.MatchValue(value=tenant_id)
+                )
+            ]
+        )
+        retriever = qdrant.as_retriever(
+            search_kwargs={'filter': filter, "k": limit, "score_threshold": score_threshold})
+        return retriever
+
+    def query(self, retriever, query):
+        return retriever.invoke(query)
--- tests/backend/app/services/test_search.py
+++ tests/backend/app/services/test_search.py
@@ -12,16 +12,9 @@ class TestSearchService(unittest.TestCase):
         self.collection_name = 'test_collection'
         self.connection = DatabaseConnection()
         self.collection = Collection(db_connection=self.connection)
-        self.VectorConfiguration = VectorConfiguration(vector_size=3, distance_metric='cosine')
+        self.VectorConfiguration = VectorConfiguration(vector_size=1536, distance_metric='cosine')
         self.collection.create_collection(self.collection_name, self.VectorConfiguration.vector_params)
         tenant_id = "test_tenant"
-        embeddings = [
-            [0.1, 0.2, 0.3],
-            [0.2, 0.3, 0.4],
-            [0.3, 0.4, 0.5],
-            [0.4, 0.5, 0.6],
-            [0.5, 0.6, 0.7]
-        ]
         docs = [
             "Hi there!",
             "Oh, hello!",
@@ -31,6 +24,7 @@ class TestSearchService(unittest.TestCase):
         ]
         self.embedding_service = EmbeddingService(model=None, embedding_service="openai",
                                                   qdrant_client=self.connection.connection)
+        embeddings = self.embedding_service.embed_documents(docs)
         self.embedding_service.store_embedding(embeddings, docs, self.collection_name, tenant_id)
 
     def tearDown(self):
@@ -38,12 +32,38 @@ class TestSearchService(unittest.TestCase):
 
     def test_search(self):
         tenant_id = "test_tenant"
-        query_vector = [0.1, 0.2, 0.3]
+        query_vector = self.embedding_service.embed_documents(["Hi there!"])[0]
         limit = 5
         searchService = SearchService()
         search_results = searchService.search(self.collection_name, tenant_id, query_vector, limit)
         self.assertIsNotNone(search_results)
         self.assertEqual(len(search_results), limit)
         closest_point = search_results[0]
-        closest_point_document = closest_point.payload['document']
+        closest_point_document = closest_point.payload['page_content']
         self.assertEqual(closest_point_document, "Hi there!")
+
+    def test_get_tenant_retriever(self):
+        tenant_id = "test_tenant"
+        searchService = SearchService()
+        retriever = searchService.get_tenant_retriever(self.collection_name, tenant_id)
+        self.assertIsNotNone(retriever)
+
+    def test_valid_query(self):
+        tenant_id = "test_tenant"
+        searchService = SearchService()
+        retriever = searchService.get_tenant_retriever(self.collection_name, tenant_id, score_threshold=0.9, limit=1)
+        query = "Hi there!"
+        query_results = searchService.query(retriever, query)
+        self.assertIsNotNone(query_results)
+        self.assertEqual(len(query_results), 1)
+
+    def test_invalid_query(self):
+        tenant_id = "test_tenant"
+        searchService = SearchService()
+        retriever = searchService.get_tenant_retriever(self.collection_name, tenant_id, score_threshold=0.9, limit=1)
+        query = "I jumped from 1000 feet!"
+        query_results = searchService.query(retriever, query)
+        self.assertIsNotNone(query_results)
+        self.assertEqual(len(query_results), 0)
+
+
